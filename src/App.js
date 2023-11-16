6a3e395265ec054c101bc0038a9d2a7f94bd5af5
Shubham Talreja <shubhamtalreja2@gmail.com>
2023-11-16 12:14:26 +0530

    removed route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="bg-[url('./assets/img/banner-bg.png')]">
-      <Router>
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Navbar />} />
         </Routes>
-      </Router>
+      </Router> */}
+      <Navbar/>
     </div>
   );
 }
