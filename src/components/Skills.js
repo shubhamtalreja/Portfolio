498d7e7e4ee0f452c487f480079bf7e744c410ea
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-09-20 23:49:16 +0530

    PF-1 Updating the portfolio code.
    - Updating the Portfolio code with current requirements.
    - Made some CSS changes.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,73 +1,66 @@
 import React from 'react'
 
 const Skills = ({ skills }) => {
-    const Lang = [
-        { progLanguage: "JavaScript" },
-        { progLanguage: "Java" },
-        { frontEnd: "HTML" },
-        { frontEnd: "CSS" },
-        { frontEnd: "JavaScript" },
-        { frontEnd: "ReactJs" },
-        { frontEnd: "BootStrap" },
-        { frontEnd: "Tailwind CSS" },
-        { backend: "NodeJs" },
-        { backend: "Express" },
-        { backend: "MongoDB" },
-        { otherSkill: "Git" },
-        { otherSkill: "AWS Amplify" },
-        { otherSkill: "AWS Lambda" },
-        { otherSkill: "AWS S3" },
-        { otherSkill: "Figma" },
-        { otherSkill: "Jira" },
-        { otherSkill: "Postman" },
-        { otherSkill: "Jenkins" },
-        { otherSkill: "Cypress" },
-        { otherSkill: "Splunk" },
-        { otherSkill: "FullStory" },
-
-    ];
+    const Lang = {
+        languages: ["JavaScript", "Java"],
+        frontend: ["HTML", "CSS", "ReactJs", "Bootstrap", "Tailwind CSS", "Shadcn"],
+        backend: ["NodeJs", "Express", "MongoDB", "DynamoDB"],
+        tools: [
+            "Git",
+            "AWS Amplify",
+            "AWS Lambda",
+            "AWS S3",
+            "Figma",
+            "Jira",
+            "Postman",
+            "Jenkins",
+            "Cypress",
+            "Splunk",
+            "FullStory"
+        ]
+    };
     return (
-        <div ref={skills} className='h-screen bg-zinc-800 rounded-xl container mx-auto p-5 opacity-70 mt-20 overflow-hidden'>
+        <div ref={skills} className=' bg-zinc-800 rounded-xl container mx-auto p-5 opacity-70 mt-20 overflow-hidden'>
         <div className='flex justify-center items-center text-gray-300 text-4xl mb-8'>
             Skills
         </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-8'>
-            <div className='text-white font-semibold'>
-                <div className='text-lg'>Languages</div>
-                <ul>
-                    {Lang.filter(item => item.progLanguage).map((item, idx) => (
+        <div className='gap-4 p-8'>
+            <div className='text-white font-semibold flex flex-row'>
+                <div className='text-lg'>Languages: </div>
+                <ul className='flex flex-row'>
+                    {Lang.languages.map((item, idx) => (
                         <li key={idx} className='border-2 border-slate-700 bg-slate-300 font-bold text-slate-800 w-fit px-2 py-1 rounded-lg my-1 text-sm'>
-                            {item.progLanguage}
+                            {item}
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className='text-white font-semibold'>
-                <div>Frontend</div>
-                <ul>
-                    {Lang.filter(item => item.frontEnd).map((item, idx) => (
+            <div className='text-white font-semibold flex flex-row'>
+                <div>Frontend:</div>
+                <ul className='flex flex-row'>
+                    {Lang.frontend.map((item, idx) => (
                         <li key={idx} className='border-2 border-slate-700 bg-slate-300 font-bold text-slate-800 w-fit px-2 py-1 rounded-lg my-1 text-sm'>
-                            {item.frontEnd}
+                            {item}
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className='text-white font-semibold'>
-                <div>Backend</div>
-                <ul>
-                    {Lang.filter(item => item.backend).map((item, idx) => (
+            <div className='text-white font-semibold flex flex-row'>
+                <div>Backend:</div>
+                <ul className='flex flex-row'>
+                    {Lang.backend.map((item, idx) => (
                         <li key={idx} className='border-2 border-slate-700 bg-slate-300 font-bold text-slate-800 w-fit px-2 py-1 rounded-lg my-1 text-sm'>
-                            {item.backend}
+                            {item}
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className='text-white font-semibold'>
-                <div>Tools</div>
-                <ul>
-                    {Lang.filter(item => item.otherSkill).map((item, idx) => (
+            <div className='text-white font-semibold flex flex-row'>
+                <div>Tools:</div>
+                <ul className='flex flex-row'>
+                    {Lang.tools.map((item, idx) => (
                         <li key={idx} className='border-2 border-slate-700 bg-slate-300 font-bold text-slate-800 w-fit px-2 py-1 rounded-lg my-1 text-sm'>
-                            {item.otherSkill}
+                            {item}
                         </li>
                     ))}
                 </ul>
